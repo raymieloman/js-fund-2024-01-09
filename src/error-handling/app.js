fb4d10153db44a5e502b061239d1cbf4e35959bd
Raymond Loman <raymond@carpago.nl>
2024-01-11 18:06:59 +0100

    Work in progress

--- src/error-handling/app.js
+++ src/error-handling/app.js
@@ -1,29 +1,19 @@
 function foo() {
     let a;
-    console.log(a.getAge());
+    console.log(a.getAge()); // throws TypeError (bewust hier om een fout te forceren)
 }
 
 function bar(n) {
-    if (n == 0) {
+    if (n == 0) { // zeg maar je mag niet door nul delen, maar in js kan delen door nul (wordt Infinity)
         throw "U mag geen nul geven";
     }
     console.log(`n is hier: ${n}`);
 }
 
-function getBankrekeningHouder(bankrekeningNummer) {
-    if (bankrekeningNummer == 0) {
-        return {
-            name: "Marcel"
-        }
-    } else {
-        return null;
-    }
-}
-
 try {
     foo();
 } catch (exception) {
-    console.log("Exception gevangen en dood gemaakt en ik ga lekker door!");
+    console.log("Exception gevangen, en prg gaat dus door");
 }
 
 bar(3);
@@ -31,15 +21,9 @@ try {
     bar(0);
 } catch (e) {
     console.log(`Er ging iets fout, kijk maar: ${e}`);
-    // throw 15;
 } finally {
+    // wordt uitgevoerd na try of na catch
     console.log("Einde try catch block");
 }
 
 console.log("Successvol einde van een leuk programma");
-
-
-let houder = getBankrekeningHouder(3);
-console.log(houder);
-houder = getBankrekeningHouder(0);
-console.log(houder);
