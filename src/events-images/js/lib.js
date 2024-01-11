30e61bca76b84ca220e6c20212e6bf34f69d6565
Raymond Loman <raymond@carpago.nl>
2024-01-11 11:52:21 +0100

    Add images

--- src/events-images/js/lib.js
+++
@@ -1,9 +0,0 @@
-function printImages() {
-    let list = document.getElementById('photos')
-    for (let i = 1; i <= 10; i++) {
-        let p = document.createElement('p')
-        p.innerHTML = `7 * ${i} = <b>${7 * i}</b>`
-        p.style.color = 'blue'
-        list.appendChild(p)
-    }
-}
