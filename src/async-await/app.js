82df52eddc9693c6cf3427578edc7001075a4e18
Raymond Loman <raymond@carpago.nl>
2024-01-11 17:55:34 +0100

    Work in progress

--- src/async-await/app.js
+++ src/async-await/app.js
@@ -1,11 +1,3 @@
-function printSquareUsingThenToUnwrapThePromise() {
-    // this uses the .then to unwrap the promise using the success and error callback handler
-    square(3).then(n => console.log(n)
-    ).catch(e => {
-        console.log(e);
-    });
-}
-
 // an async function returns a Promise
 async function square(n) {
     return n ** 2;
@@ -16,7 +8,6 @@ async function printSquare() {
     console.log(await square(3)); // the await waits for the promise to complete, unwraps and continues
 }
 
-printSquareUsingThenToUnwrapThePromise();
 printSquare();
 
 console.log("Einde programma")
